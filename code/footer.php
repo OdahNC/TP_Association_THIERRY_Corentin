ea114824d49f095f56fc58ac2f87e761f1d84d83
OdahNC <112839326+OdahNC@users.noreply.github.com>
2023-06-13 22:02:17 +1100

    Modification MCD et BDD + Modif mineure dans le code

---
+++ code/footer.php
@@ -0,0 +1,3 @@
+Ceci est le footer
+</body>
+</html>
--- code/ouvrages.php
+++ code/ouvrages.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
+<!-- Intégration du header -->
+<?php require_once('header.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/ouvrages.css?<?= time() ?>">
+<button class="btn btn-danger">TEST</button>
 
-    <!-- Intégration de Bootstrap 5.3 via CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <!-- ************************************ -->
-
-    <title>Association</title>
-</head>
-
-<body>
-    <h1>Les ouvrages</h1>
-
-    <?php echo 'bonjour !' ?>
-
-    <a href="index.php" class="btn btn-primary">Accueil</a>
-</body>
-
-</html>
+<!-- Intégration du footer -->
+<?php require_once('footer.php') ?>
